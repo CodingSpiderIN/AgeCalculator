33cb02ee950bc2f0f45016226eecbbfa83b90ac1
phpspiderr <phpspiderr@gmail.com>
2022-10-26 08:21:31 +0530

    Updated

--- app/src/main/java/in/codingspider/agecalculator/MainActivity.kt
+++ app/src/main/java/in/codingspider/agecalculator/MainActivity.kt
@@ -1,26 +1,58 @@
 package `in`.codingspider.agecalculator
 
-import android.icu.util.Calendar
-import androidx.appcompat.app.AppCompatActivity
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var today_date: EditText
+     lateinit var today_date: EditText
+    lateinit var dob: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         today_date = findViewById(R.id.todayDate)
-
         val sdf = SimpleDateFormat("dd/MM/yyyy")
+        today_date.setText(sdf.format(Date()))
+        // on below line we are initializing our variables.
+        dob = findViewById(R.id.date_of_birth)
 
+        // on below line we are adding
+        // click listener for our edit text.
+        dob.setOnClickListener {
 
+            // on below line we are getting
+            // the instance of our calendar.
+            val c = Calendar.getInstance()
 
+            // on below line we are getting
+            // our day, month and year.
+            val year = c.get(Calendar.YEAR)
+            val month = c.get(Calendar.MONTH)
+            val day = c.get(Calendar.DAY_OF_MONTH)
 
-        today_date.setText ( sdf.format(Date()))
-
+            // on below line we are creating a
+            // variable for date picker dialog.
+            val datePickerDialog = DatePickerDialog(
+                // on below line we are passing context.
+                this,
+                { view, year, monthOfYear, dayOfMonth ->
+                    // on below line we are setting
+                    // date to our edit text.
+                    val dat = (dayOfMonth.toString() + "-" + (monthOfYear + 1) + "-" + year)
+                    dob.setText(dat)
+                },
+                // on below line we are passing year, month
+                // and day for the selected date in our date picker.
+                year,
+                month,
+                day
+            )
+            // at last we are calling show
+            // to display our date picker dialog.
+            datePickerDialog.show()
+        }
     }
 }
